64fbb6faabaae9139d52dd15972d76d787fe3ea4
TM1744 <marquesthiago1744@gmail.com>
2026-06-08 19:55:10 -0300

    primeiro commit

---
+++ js/login.js
@@ -0,0 +1,17 @@
+var body = document.querySelector("body")
+var singInButton = document.querySelector("#singIn")
+var singUpButton = document.querySelector("#singUp")
+
+body.onload = function()
+{
+    body.className = "on-load"
+}
+
+singInButton.addEventListener("click", function()
+{
+    body.className = "sing-in"
+});
+
+singUpButton.addEventListener("click", function() {
+    body.className = "sing-up"
+})
